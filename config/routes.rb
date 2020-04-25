5982b4906195eff13f3c93c33892cda48194ad6a
takalivian <61145524+takalivian@users.noreply.github.com>
2020-04-25 12:58:18 +0900

    list-root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :todo_lists, only: [:index, :create]
+  root to: "todo_lists#index"
 end
